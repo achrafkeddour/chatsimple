c6fdd90b2cfee9194685cce952d91ac116740dbd
achrafkeddour <152210432+achrafkeddour@users.noreply.github.com>
2024-07-03 15:13:31 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`check localhost or Ur ip address on port ${PORT} in web`);
+});
